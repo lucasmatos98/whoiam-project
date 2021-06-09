730ea3acd6489ba07b4b730a3a090dbe71469828
lucasmatos98 <cdxlucas@gmail.com>
2021-06-09 09:05:23 -0300

    feat: whoiam added

---
+++ src/lib.ts
@@ -0,0 +1,8 @@
+
+/**
+ * shortcut for console.log(obj)
+ * @param {any} obj item to be printed
+ */
+export function print(obj: any): void {
+    console.log(obj);
+}
--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,8 @@
 require('dotenv').config();
 import express = require('express');
 const app = express();
-
+// eslint-disable-next-line no-unused-vars
+import { print } from './lib';
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 import cors = require('cors');
@@ -14,24 +15,30 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+    // next line needed to remove dist/ folder from the URL
+    const localfolder = __dirname.slice(0, __dirname.length - 5);
+    res.sendFile(localfolder + '/views/index.html');
 });
 
 
 // your first API endpoint...
-app.get('/api/hello', (req, res) => {
-    res.json({ greeting: 'hello API' });
+app.get('/api/whoami', (req, res) => {
+    res.json({
+        language: req.headers['accept-language'],
+        ip: req.ip,
+        software: req.headers['user-agent'],
+    });
 });
 
 
 // listen for requests :)
-// import { AddressInfo } from 'net';
-// const listener = app.listen(process.env.PORT, () => {
-//     const { port } = listener.address() as AddressInfo;
-//     console.log('Your app is listening on port ' + port);
-// });
-
-const devPort = 3000;
-app.listen(devPort, () => {
-    console.log('Your app is listening on port ' + devPort);
+import { AddressInfo } from 'net';
+const listener = app.listen(process.env.PORT, () => {
+    const { port } = listener.address() as AddressInfo;
+    console.log('Your app is listening on port ' + port);
 });
+
+// const devPort = 3000;
+// app.listen(devPort, () => {
+//     console.log('Your app is listening on port ' + devPort);
+// });
